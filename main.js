f37aae8b07f203cadfdfb22004570862a0fb92b2
Victor Kindrat <kindratvictor07@gmail.com>
2022-06-05 17:21:31 +0300

    changes

--- main.js
+++ main.js
@@ -17,15 +17,4 @@ tippy('#button', {
     allowHTML: true
 });
 
-const deviceType = () => {
-    const ua = navigator.userAgent;
-    if (/(tablet|ipad|playbook|silk)|(android(?!.*mobi))/i.test(ua)) {
-        return "tablet";
-    }
-    else if (/Mobile|Android|iP(hone|od)|IEMobile|BlackBerry|Kindle|Silk-Accelerated|(hpw|web)OS|Opera M(obi|ini)/.test(ua)) {
-        return "mobile";
-    }
-    return "desktop";
-};
-
 alert(deviceType());
